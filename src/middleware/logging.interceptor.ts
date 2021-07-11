accfa932091d234cc7f7db1226e664e71dd0578d
Maxim Kalinin <kalinkin.fiz@gmail.com>
2021-07-11 03:03:58 +0300

    fix: logger

--- src/middleware/logging.interceptor.ts
+++ src/middleware/logging.interceptor.ts
@@ -1,8 +1,8 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor, Logger } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
-// import * as path from 'path';
-// import * as fs from 'fs';
+import * as path from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
@@ -12,11 +12,24 @@ export class LoggingInterceptor implements NestInterceptor {
 
     const requestTime = new Date();
     const processTime = Date.now() - +requestTime;
-    // const logsFolder = path.join(__dirname, '../../logs');
+    const logsFolder = path.join(__dirname, '../../logs');
 
-    // if (!fs.existsSync(logsFolder)) {
-    //   fs.mkdirSync(logsFolder);
-    // }
+    if (!fs.existsSync(logsFolder)) {
+      fs.mkdirSync(logsFolder);
+    }
+
+    const recLogging = (text: string) => {
+      fs.appendFileSync('./logs/logging.log', `${text}\n`);
+    };
+
+    recLogging(
+      ` ⛩   REQUEST: ${req.method} 
+      URL: ${req.url}, 
+      QUERY: ${req.query}, 
+      BODY: ${req.body} 
+      PROCESSING TIME: ${processTime} 
+      STATUS CODE: ${res.statusCode}\n`,
+    );
 
     return next.handle().pipe(
       tap(() => {
